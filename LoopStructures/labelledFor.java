83e2f0b97b0a5ac0a6846e1a430f2d262af9e48a
Aayush-Sahay <aayushsahaywork@gmail.com>
2025-08-16 13:40:53 +0530

    Added program 4 Labelled for loop

---
+++ LoopStructures/labelledFor.java
@@ -0,0 +1,17 @@
+package LoopStructures;
+
+public class labelledFor {
+    public static void main(String args[]){
+        int i,j;
+        A1:
+        for(i=1;i<=5;i++){
+            for(j=1;j<=5;j++){
+                System.out.print(j);
+                if(j==3)
+                    break A1;
+            }
+            System.out.println();
+        }
+    }
+}
+//Output :123
