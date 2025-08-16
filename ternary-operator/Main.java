eef7ee325eb58ba600b3a7bc973d6a655d0b1c19
Aayush-Sahay <aayushsahaywork@gmail.com>
2025-08-16 11:57:46 +0530

    Adding program Ternary operator

---
+++ ternary-operator/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String args[]){
+        System.out.println("Greatest among 2 numbers :");
+        int a =10, b=20;
+        int bigger = (a>b)?a:b;
+        System.out.println("Bigger no. is ="+bigger);
+    }
+
+}
+
+//output results
+// Greates among 2 numbers :
+// Bigger no. is =20
