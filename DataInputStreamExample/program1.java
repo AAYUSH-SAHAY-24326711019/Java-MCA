38a2599c31c9ec7c77b8c6078ada5c028a65ab68
Aayush-Sahay <aayushsahaywork@gmail.com>
2025-08-18 20:30:07 +0530

    Data Input Stream Example

---
+++ DataInputStreamExample/program1.java
@@ -0,0 +1,19 @@
+package DataInputStreamExample;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+public class program1 {
+    public static void main(String args[]) throws IOException{
+        DataInputStream obj = new DataInputStream(System.in);
+        int a ,b;
+        a= Integer.parseInt(obj.readLine());
+        b= Integer.parseInt(obj.readLine());
+        System.out.println("The sum of ("+a+"+"+b+") is ="+(a+b));
+    }
+    
+}
+// Test 1
+// 10
+// 20
+// The sum of (10+20) is =30
