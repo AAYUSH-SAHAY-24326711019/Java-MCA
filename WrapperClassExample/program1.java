1623bd887a26b0e5a2ac36d65f24cd16aa204e69
Aayush-Sahay <aayushsahaywork@gmail.com>
2025-08-18 20:40:46 +0530

    Adding Wrapper Class Program

---
+++ WrapperClassExample/program1.java
@@ -0,0 +1,26 @@
+package WrapperClassExample;
+import java.io.StringReader;
+import java.util.Scanner;
+
+public class program1 {
+    public static void main(String args[]){
+        Scanner z = new Scanner(System.in);
+        Integer a,b;
+        System.out.println("User, Enter 2 numbers:");
+        a= z.nextInt();
+        b=z.nextInt();
+        System.out.print("The sum is ="+(a+b)+"\n");
+
+        String name ;
+        name = z.next();
+        System.out.printf("User, Your name is %s",name);
+
+    }
+}
+// Test 1
+// User, Enter 2 numbers:
+// 10
+// 20
+// The sum is =30
+// Aayush Sahay
+// User, Your name is Aayush
