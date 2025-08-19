4131660ad728411869cbc40db0ba5357f9838441
Aayush-Sahay <aayushsahaywork@gmail.com>
2025-08-19 11:59:26 +0530

    Adding program having multiple classes

---
+++ Extra/A.java
@@ -0,0 +1,13 @@
+package Extra;
+
+class A {
+    public static void main(String args[]){
+        System.out.println("Hello India");
+    }
+}
+
+class B{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
