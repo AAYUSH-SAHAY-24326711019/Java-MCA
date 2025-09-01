17f4f92ad6f1b1e830dc4024faef2e6e7da3ae76
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2025-09-01 21:42:29 +0530

    Adding files for the Practical of the Packages in java-2

---
+++ mypackage/A.java
@@ -0,0 +1,8 @@
+package mypackage;
+
+public class A{
+
+    public void show(){
+        System.out.println("Hello this is Show function");
+    }
+}
---
+++ mypackage/B.java
@@ -0,0 +1,8 @@
+package mypackage;
+
+public class B{
+
+    public void display(){
+        System.out.println("Hello this is Display function");
+    }
+}
