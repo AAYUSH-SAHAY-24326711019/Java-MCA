41eb5d6fe42fb0ec77b11bfe57ed7703acb5c139
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2025-09-01 21:41:47 +0530

    Adding files for the Practical of the Packages in java

---
+++ TestPackage/newTest.java
@@ -0,0 +1,17 @@
+package TestPackage;
+import mypackage.*;
+public class newTest{
+    public static void main(String args[]){
+        A p = new A();
+        B q = new B();
+
+        p.show();
+        q.display();
+        
+    }
+}
+//
+// PS C:\Users\Dell\Desktop\Daily\Java-MCA> cd .\TestPackage\
+// PS C:\Users\Dell\Desktop\Daily\Java-MCA\TestPackage> java .\newTest.java
+// Hello this is Show function
+// Hello this is Display function
