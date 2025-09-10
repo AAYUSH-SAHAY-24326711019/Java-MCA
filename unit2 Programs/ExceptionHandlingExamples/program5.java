03745edfa755ef0a57a03897bb1f7a56ddb6fa3c
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2025-09-10 08:06:21 +0530

    Adding Program 5 for Number format Exception

---
+++ unit2 Programs/ExceptionHandlingExamples/program5.java
@@ -0,0 +1,29 @@
+package ExceptionHandlingExamples;
+
+/*We will make the program to fix the Number Format Exception
+ * This will occur when a string is being type converted into an
+ * integer
+ */
+
+public class program5 {
+    public static void main(String[] args) {
+        String demo = "Demo-String";
+        int k =0;
+        try{
+            int demo_int = Integer.parseInt(demo);
+            System.out.println("The value inside demo_int is :"+demo_int);
+        }catch(Exception e){
+            System.out.println("Raised Exception : "+e);
+            k=1;
+        }
+        if(k==1){
+            System.out.println("Completed with exceptions");
+        }else{
+            System.out.println("Successfull termination of program");
+        }
+    }
+    
+}
+// output
+// Raised Exception : java.lang.NumberFormatException: For input string: "Demo-String"
+// Completed with exceptions
