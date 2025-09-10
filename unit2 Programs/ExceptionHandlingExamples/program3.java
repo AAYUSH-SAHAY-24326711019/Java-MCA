d0762cf8294af97cece628fe8b33f70ac9ef05e4
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2025-09-10 07:31:39 +0530

    Specifying the types of Errors and Exceptions

---
+++ unit2 Programs/ExceptionHandlingExamples/program3.java
@@ -0,0 +1,30 @@
+/*
+ * Throwable class has 2 children Exception and Error.
+ * Exception : logical Error.(Many types of exceptions)
+ * Error : non User responsible error , like stack memory overflow error,linkage error , 
+ * out of memory error
+ * 
+ * 
+ * Main exceptions are:
+ * 1. runtime exception 
+ *      Arithematic exception
+ *      Nullpointer exception
+ *      NumberFormat exception
+ * IndexOutofBounds Exception
+ *      ArrayIndexOutOfBounds Exception
+ *      StringIndexOutOfBounds Exception
+ * 
+ * 2. IOException
+ *      EOFException (End of file exception)
+ *      FileNotFoundException
+ * 
+ * 3. SQLException
+ * 4. InterruptedException
+ * 5. ClassNotFoundError
+ *  
+ * 
+ * Errors and Exception are not found in compile time, During the interpretation by jvm
+ * They will arise.
+ * 
+ * User gets 5 features : try , catch , throw , throws , finally.
+ */
