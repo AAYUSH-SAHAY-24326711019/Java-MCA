6a89104dfc0f0df90b268625e4bf962361e4ec13
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2025-09-18 21:27:39 +0530

    Adding program 1 for basics of java internet programming

---
+++ Unit3/program1.java
@@ -0,0 +1,40 @@
+package Unit3;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.io.IOException;
+
+public class program1{
+    public static void main(String[] args) {
+        try {
+            //getting details of the local machine
+            InetAddress localHost = InetAddress.getLocalHost();
+            System.out.println("Local Host name :"+localHost.getHostName());
+            System.out.println("Local Host IP address :"+localHost.getHostAddress());
+            //getHostAddress() return string value for ip address
+
+            //getting details of remote connected device
+            InetAddress msAddress = InetAddress.getByName("www.microsoft.com");
+            System.out.println("IP address of site :"+msAddress.getHostAddress());
+
+            //to check whether remote server reachable or not
+            if(msAddress.isReachable(5000)){
+                System.out.println("Host is reachable");
+            }else{
+                System.out.println("Host is un-reachable");
+            }
+        } catch (UnknownHostException e) {
+            e.getMessage();
+        }
+        catch(IOException ie){
+            ie.getMessage();
+        }
+    }
+}
+
+/*output
+ * 
+Local Host name :AayushLenovo
+Local Host IP address :192.168.56.1
+IP address of site :23.0.254.92
+Host is reachable
+ */
