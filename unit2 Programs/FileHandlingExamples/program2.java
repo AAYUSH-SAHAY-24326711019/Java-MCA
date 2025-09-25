811ab1b759622b47a8cff83273703dbe61bf47d7
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2025-09-25 17:36:47 +0530

    Adding program 2 to fetch the details of the file

---
+++ unit2 Programs/FileHandlingExamples/program2.java
@@ -0,0 +1,34 @@
+package FileHandlingExamples;
+
+import java.io.File;
+import java.io.IOException;
+
+public class program2 {
+    public static void main(String[] args) {
+        try{
+            File f = new File("C:\\Users\\Lenovo\\Desktop\\Java-MCA\\unit2 Programs\\FileHandlingExamples\\File-Sandbox\\create_file1.txt");
+            if(f.exists()){
+                System.out.println("File-Name : "+f.getName());
+                System.out.println("Location of file : "+f.getPath());
+                System.out.println("Absolute path "+f.getAbsolutePath());
+                System.out.println("Readable ? : "+f.canRead());
+                System.out.println("Writable? : "+f.canWrite());
+                System.out.println("Executable ? : "+f.canExecute());
+                System.out.println("Length of file : "+f.length());
+            }
+            else{
+                System.out.println("File not exist...");
+            }
+        }catch(Exception ioe){
+            System.out.println(ioe.getMessage());
+        }
+    }
+}
+/*output */
+// File-Name : create_file1.txt
+// Location of file : C:\Users\Lenovo\Desktop\Java-MCA\unit2 Programs\FileHandlingExamples\File-Sandbox\create_file1.txt
+// Absolute path C:\Users\Lenovo\Desktop\Java-MCA\unit2 Programs\FileHandlingExamples\File-Sandbox\create_file1.txt
+// Readable ? : true
+// Writable? : true
+// Executable ? : true
+// Length of file : 0
